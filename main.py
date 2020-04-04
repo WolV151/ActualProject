3d9eac6fe59141ba1a1fc15e09823681ae3e1c48
WolV151 <marindontchev@gmail.com>
2020-04-04 12:00:07 +0300

    Created 2 more functions, one that reads all module names from a file and another one to display them in the main menu

--- main.py
+++ main.py
@@ -12,21 +12,46 @@ def main_menu():
                                                    "3. Exit\n"
                                                    "=====> ")
 
-        if user_number == 1:
-            print("Record attendance")
-            break
+        if user_number == 1:  # return use choice
+            return 1
         elif user_number == 2:
-            print("Generate stats")
-            break
+            return 2
         elif user_number == 3:
             exit("Main exited")
-        else:
+        else:  # validation
             print("Option out range, please select a valid option from the main")
 
 
+def list_modules():  # module display
+    module_codes = []
+    module_names = []
+
+    with open("modules.txt", "r") as readfile:
+        for read_pointer in readfile:
+            split_text = read_pointer.split(",")
+
+            module_codes.append(split_text[0].strip())
+            module_names.append(split_text[1].strip())
+
+    return module_codes, module_names
+
+
+def module_menu(module_codes, module_names):
+    print("\nModule Record System (Attendance) - Choose a module\n"
+          "===================================================")
+
+    for i, x in enumerate(module_codes):
+        print(f"{i+1}. {module_codes[i]} - {module_names[i]}")
+
+    user_number = mod.read_nonnegative_integer("=====> ")
 
 
 def main():
-    main_menu()
+    user_choice = main_menu()
+    module_codes, module_names = list_modules()
+    module_menu(module_codes, module_names)
+
+
+
 
 main()
